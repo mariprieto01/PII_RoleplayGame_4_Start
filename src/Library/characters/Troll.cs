19476b6d8b7df8dfbb240725ba271135e099c729
Mateo Mederos <mateomederos@192.168.1.7>
2023-05-28 19:14:26 -0300

    Agrego Troll, Coraza y Palo

---
+++ src/Library/characters/Troll.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using RoleplayGame.Items;
+
+namespace RoleplayGame.Characters
+{
+    /// <summary>
+    /// Personaje Troll
+    /// </summary>
+    public class Troll : Character
+    {
+        public Troll(string name)
+            : base(name)
+        {
+            this.Health = 180;
+            this.AddItem(new Palo());
+            this.AddItem(new Coraza());
+        }
+    }
+}
