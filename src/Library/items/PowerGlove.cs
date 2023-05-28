3a5adda37179eed4d523a0b392e291ea5aa27727
mariprieto01 <noel.prieto@correo.ucu.edu.uy>
2023-05-28 20:11:21 -0300

    Add PowerGlove

---
+++ src/Library/items/PowerGlove.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+
+namespace RoleplayGame.Items
+{
+    public class PowerGlove : IAttackItem
+    {
+        public int attackPower;
+        public int AttackPower
+        {
+            get
+            {
+                return attackPower;
+            }
+        }
+        public PowerGlove()
+        {
+            attackPower = 0;
+        }
+        public void AddGem(Gem gem)
+        {
+            attackPower += gem.AttackPower;
+        }
+
+        public override string ToString()
+        {
+            return "PowerGlove";
+        }
+    }
+}
